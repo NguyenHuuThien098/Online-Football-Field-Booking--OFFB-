c31bdf707758e06b1cb85af2274235c7b7fbeff5
huuthang0218 <huut789@gmail.com>
2024-11-01 22:48:54 +0700

    Cập nhật file User.js

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -28,6 +28,31 @@ class User {
             throw new Error('Could not retrieve user');
         }
     }
+
+    // Cập nhật thông tin người dùng
+    static async updateUser(uid, userData) {
+        await admin.database().ref(`users/${uid}`).update(userData);
+    }
+
+    // Lấy thông tin người dùng
+    static async getUserById(uid) {
+        const snapshot = await admin.database().ref(`users/${uid}`).once('value');
+        if (!snapshot.exists()) {
+            throw new Error('User does not exist');
+        }
+        return { uid: uid, ...snapshot.val() };
+    }
+
+    // Xác thực token Google
+    static async verifyGoogleToken(token) {
+        try {
+            const decodedToken = await admin.auth().verifyIdToken(token);
+            return decodedToken; // Trả về thông tin người dùng nếu token hợp lệ
+        } catch (error) {
+            console.error('Error verifying Google token:', error);
+            throw new Error('Invalid token');
+        }
+    }
 }
 
 module.exports = User;
