425edb79828f02f22197b9375dfa576fa4b56ac6
TranVuThanhLam <thanhlam20062003@gmail.com>
2024-11-12 18:56:09 +0700

    commit fix loi git

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,19 +3,13 @@ import MainLayout from "../layouts/MainLayout";
 import Header from "../components/common/Header";
 import Sidebar from "../components/common/Sidebar";
 import SearchTool from "../components/common/SearchTool";
-import Item from "../components/common/Item"
 
 const Home = () => {
   
   return (
     <MainLayout>
       <SearchTool/>
-      <Item/>
-      <Item/>
-      <Item/>
-      <Item/>
-      <Item/>
-      <Item/>
+        <item/>
     </ MainLayout>
   );
 };
