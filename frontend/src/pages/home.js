01f18d093b8cc80d5cf4dac0a686bbb57cd35913
TranVuThanhLam <thanhlam20062003@gmail.com>
2024-11-12 19:37:45 +0700

    mini update

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import MainLayout from "../layouts/MainLayout";
-import Header from "../components/common/Header";
-import Sidebar from "../components/common/Sidebar";
 import SearchTool from "../components/common/SearchTool";
+import Item from "../components/common/Item"
 
 const Home = () => {
   
   return (
     <MainLayout>
       <SearchTool/>
-        <item/>
+        <Item/>
+        <Item/>
+        <Item/>
     </ MainLayout>
   );
 };
